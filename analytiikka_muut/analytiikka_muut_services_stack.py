5d9da6599c7e4d75eb66f9c7751d42d5eb960857
isto.saarinen <isto.saarinen@cgi.com>
2023-11-20 12:48:42 +0200

    cdk app pohja, korjaukset

--- analytiikka_muut/analytiikka_muut_services_stack.py
+++ analytiikka_muut/analytiikka_muut_services_stack.py
@@ -58,7 +58,7 @@ class AnalytiikkaMuutServicesStack(Stack):
 
 
         # glue_securitygroup = aws_ec2.SecurityGroup.from_lookup_by_name(self, "GlueSecurityGroup", security_group_name = glue_security_group_name, vpc = vpc)
-        glue_role = aws_iam.Role.from_role_arn(self, "GlueRole", f"arn:aws:iam::{self.account}:role/{glue_role_name}", mutable=False)
+        ## glue_role = aws_iam.Role.from_role_arn(self, "GlueRole", f"arn:aws:iam::{self.account}:role/{glue_role_name}", mutable=False)
 
         # glue_script_bucket = aws_s3.Bucket(self,
         #                                    id = glue_script_bucket_name,
@@ -161,7 +161,7 @@ class AnalytiikkaMuutServicesStack(Stack):
                  description = "Glue jobin kuvaus",
                  worker = "G.1X",
                  version = "4.0",
-                 role = glue_role,
+                 role = glue_role_name,
                  tags = None,
                  arguments = None,
                  connections = None
